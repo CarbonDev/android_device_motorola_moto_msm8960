b7e2b1fd45c5c8fb4b96e1a091dafc4c5084dc61
arrrghhh <arrrghhh@xdandroid.com>
2014-02-14 03:21:58 -0700

    Missed cm-->Carbon

--- carbon.mk
+++ carbon.mk
@@ -6,6 +6,6 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
-PRODUCT_NAME := cm_moto_msm8960
+PRODUCT_NAME := carbon_moto_msm8960
 
 $(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
